07e982be49018aac6526233d37638c4308e9cc5e
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-03-09 16:57:23 +0100

    Updates
    Thanks to Thomas for the input for two url's

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ import express from "express";
 
 // Create a new Express app
 const app = express();
-const url = "https://raw.githubusercontent.com/fdnd-agency/ultitv/main/api/game/943.json";
-const stats =
-  "https://raw.githubusercontent.com/fdnd-agency/ultitv/main/api/game/943/statistics.json";
-
+// const url = "https://raw.githubusercontent.com/fdnd-agency/ultitv/main/api/game/943.json";
 // const url = "https://github.com/fdnd-agency/ultitv/blob/main/api/game/943.json";
+
+const urls = [
+  "https://raw.githubusercontent.com/fdnd-agency/ultitv/main/api/game/943.json",
+  "https://raw.githubusercontent.com/fdnd-agency/ultitv/main/api/game/943/statistics.json",
+];
 
 // Set EJS as the template engine and specify the views directory
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Serve static files from the public directory
 app.use(express.static("public"));
 
 // Create a route for the index page
-app.get("/", function (request, response) {
-  fetchJson(url).then((data) => {
-    response.render("index", data);
-    console.log(data);
-  });
-});
+// app.get("/", function (request, response) {
+//   fetchJson(url).then((data) => {
+//     response.render("index", data);
+//     console.log(data);
+//   });
+// });
 
-app.get("/", function (request, response) {
-  fetchJson(stats).then((data) => {
-    response.render("index", data);
-    console.log(data);
-  });
+app.get("/", async function (request, response) {
+  const [data1, data2] = await Promise.all(urls.map(fetchJson));
+  const data = { data1, data2 };
+  response.render("index", data);
+
+  console.log(data);
 });
 
 // Set the port number and start the server
@@ -37,9 +43,9 @@ app.listen(port, () => {
   console.log(`Application available on: http://localhost:${port}`);
 });
 
-async function fetchJson(url) {
-  return await fetch(url)
+async function fetchJson(urls) {
+  return await fetch(urls)
     .then((response) => response.json())
     .catch((error) => error);
-  console.log(url);
+  console.log(urls);
 }
